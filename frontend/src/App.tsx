2ea3bf4e2e17a359e39bd1de7053e780c8db93f1
kykapple <kykapple@naver.com>
2021-12-28 01:02:16 +0900

    feat: font 추가 구현 - webpack css-loader 추가 - 최상단인 index.tsx에 font.css 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const App = () => <div></div>;
+import Login from './pages/Login';
+
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Login} />
+        </Switch>
+    </Router>
+);
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import './styles/font.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
