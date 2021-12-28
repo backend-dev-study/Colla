88bf6da3259b227002264ad782fc24ef74138ef5
kykapple <kykapple@naver.com>
2021-12-29 01:27:51 +0900

    fix: auth api path 수정

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getAccessToken = async (code: string) => {
-    const response = await axios.get<{ accessToken: string }>(`${process.env.API_URL}?code=${code}`);
+    const response = await axios.get<{ accessToken: string }>(`${process.env.API_URL}/auth/login?code=${code}`);
 
     return response.data;
 };
