74d3c99f8743072d1cdd7285d6a9663b15fcc35b
kykapple <kykapple@naver.com>
2021-12-28 19:28:35 +0900

    feat: github login callback 과정 구현

---
+++ frontend/src/pages/LoginProcessing/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import { Center } from '../../styles/common';
+
+export const Container = styled.div`
+    ${Center}
+    flex-direction: column;
+    margin: auto 0;
+`;
+
+export const Notice = styled.div`
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 50px;
+    color: #fff;
+`;
