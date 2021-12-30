47ae73a77aac2624d1688b89537f0e63809841ed
kykapple <kykapple@naver.com>
2021-12-30 21:54:19 +0900

    feat: 올바르지 않은 OAuth code가 온 경우 예외 처리 구현

--- backend/src/main/java/kr/kro/colla/auth/presentation/dto/GithubLoginResponse.java
+++ backend/src/main/java/kr/kro/colla/auth/presentation/dto/GithubLoginResponse.java
@@ -1,12 +1,19 @@
 package kr.kro.colla.auth.presentation.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class GithubLoginResponse {
 
     private String accessToken;
+
+    private String message;
+
+    @Builder
+    public GithubLoginResponse(String accessToken, String message) {
+        this.accessToken = accessToken;
+        this.message = message;
+    }
 
 }
