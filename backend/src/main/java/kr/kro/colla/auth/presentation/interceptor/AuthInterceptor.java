96e927c3fb578e4c4bbd0f4b0d2c187a5c7ad4ae
kykapple <kykapple@naver.com>
2022-01-04 21:27:58 +0900

    refactor: 함수 파라미터 개선

--- backend/src/main/java/kr/kro/colla/auth/presentation/interceptor/AuthInterceptor.java
+++ backend/src/main/java/kr/kro/colla/auth/presentation/interceptor/AuthInterceptor.java
@@ -19,18 +19,19 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
-        Cookie accessToken = parseCookies(cookies);
+        Cookie accessToken = parseCookies(cookies, "accessToken");
 
         if(accessToken == null || jwtProvider.validateToken(accessToken.getValue())) {
             return false;
         }
 
+        request.setAttribute("accessToken", accessToken.getValue());
         return true;
     }
 
-    private Cookie parseCookies(Cookie[] cookies) {
+    private Cookie parseCookies(Cookie[] cookies, String name) {
         return Arrays.stream(cookies)
-                .filter(cookie -> cookie.getName().equals("accessToken"))
+                .filter(cookie -> cookie.getName().equals(name))
                 .findAny()
                 .orElse(null);
     }
