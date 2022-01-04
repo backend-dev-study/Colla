eea1b2398139456efe2c50429f4f2a300fb54fb9
kykapple <kykapple@naver.com>
2022-01-04 17:50:15 +0900

    refactor: 로그인 로직 리팩토링 - 로그인 성공 시 status code로 판별 - 공통으로 사용되는 api들 common.ts에 구현

--- frontend/src/apis/user.ts
+++ frontend/src/apis/user.ts
@@ -1,4 +1,4 @@
-import client from './client';
+import { client } from './common';
 
 export const createProject = async (userId: number, projectName: string, projectDesc: string) => {
     const response = await client.post(`/users/${userId}/projects`, { name: projectName, description: projectDesc });
