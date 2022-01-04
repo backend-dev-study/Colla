b88ab702f95ceed9de37a37e42973e4f9280095a
kykapple <kykapple@naver.com>
2022-01-04 17:15:27 +0900

    feat: access token 쿠키 전송 및 cors 해결

--- backend/src/main/java/kr/kro/colla/auth/presentation/AuthController.java
+++ backend/src/main/java/kr/kro/colla/auth/presentation/AuthController.java
@@ -1,37 +1,49 @@
 package kr.kro.colla.auth.presentation;
 
-import kr.kro.colla.auth.presentation.dto.GithubLoginResponse;
 import kr.kro.colla.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@CrossOrigin(value = "*")
 @RequestMapping("/auth")
 @RestController
 public class AuthController {
 
+    @Value("${cookie.domain}")
+    private String credentialDomain;
+
+    @Value("${cookie.expiration_time}")
+    private long expirationTime;
+
     private final AuthService authService;
 
     @GetMapping("/login")
-    public ResponseEntity<GithubLoginResponse> githubLogin(@RequestParam String code) {
+    public ResponseEntity githubLogin(@RequestParam String code) {
         String accessToken = this.authService.githubLogin(code);
 
         if(accessToken == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                    GithubLoginResponse.builder()
-                            .message("permission denied")
-                            .build()
-            );
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .build();
         }
 
-        return ResponseEntity.ok(
-                GithubLoginResponse.builder()
-                        .accessToken(accessToken)
-                        .build()
-        );
+        ResponseCookie cookie = createCookie(accessToken);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
+    }
+
+    private ResponseCookie createCookie(String value) {
+        return ResponseCookie.from("accessToken", value)
+                .maxAge(expirationTime)
+                .domain(credentialDomain)
+                .path("/")
+                .build();
     }
 
 }
