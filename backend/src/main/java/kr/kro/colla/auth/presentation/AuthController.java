a2908b7f644d88fa5fdff56165f796d9158039a2
Subin Min <tnqls5417@naver.com>
2022-01-05 00:47:13 +0900

    feat: 인증시 예외 ExceptionHandler 처리
    - AuthPermissionDeniedException 추가
    - 해당 예외 케이스 ExceptionHandler에 추가

--- backend/src/main/java/kr/kro/colla/auth/presentation/AuthController.java
+++ backend/src/main/java/kr/kro/colla/auth/presentation/AuthController.java
@@ -2,6 +2,7 @@ package kr.kro.colla.auth.presentation;
 
 import kr.kro.colla.auth.presentation.dto.GithubLoginResponse;
 import kr.kro.colla.auth.service.AuthService;
+import kr.kro.colla.error.exception.auth.AuthPermissionDeniedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +21,7 @@ public class AuthController {
         String accessToken = this.authService.githubLogin(code);
 
         if(accessToken == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                    GithubLoginResponse.builder()
-                            .message("permission denied")
-                            .build()
-            );
+            throw new AuthPermissionDeniedException();
         }
 
         return ResponseEntity.ok(
--- backend/src/main/java/kr/kro/colla/error/GlobalExceptionHandler.java
+++ backend/src/main/java/kr/kro/colla/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.kro.colla.error;
 
+import kr.kro.colla.error.exception.auth.AuthPermissionDeniedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,6 +15,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
         FieldError fieldError = e.getFieldError();
         ErrorResponse response = new ErrorResponse(400, fieldError.getField()+" : "+fieldError.getDefaultMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(AuthPermissionDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAuthPermissionDeniedException(AuthPermissionDeniedException e){
+        ErrorResponse response = new ErrorResponse(400, e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
