87fed26bd8e47f58ae8386571d2da3e368c1c975
민수빈 <tnqls5417@naver.com>
2022-01-06 14:05:52 +0900

    [#31] feat: 프로젝트 정보 전역 상태값 설정 (#34)
    * chore: Recoil dependency 추가
    
    * feat: prefilght 허용하도록 interceptor 변경
    
    - prefilght OPTIONS 메서드 허용 안할시 CORS 에러 처리됨
    - Project 이름 '' empty 허용하지 않도록 valid 조건 변경
    
    * refactor: api 함수 반환 데이터 수정
    
    * feat: Recoil 전역 상태값 추가
    
    - 프로젝트 관련 전역 상태값 설정
    - atom으로 전역 state 추가
    - 묶어서 가져오기 위한 selector 추가
    
    * feat: 프로젝트 Atom UI에 반영
    
    - 프로젝트 추가시 해당 프로젝트 정보 atom 값 set
    - Header 프로젝트 atom 값과 왼쪽 영역 연동
    
    * chore: @emotion/babel-plugin 설정 추가
    
    - Component selector 에러 해결
    - Parent = styled.div` ${Child}{}` 방식 가능
    
    * feat: 헤더 스타일 수정
    
    - hover시에 프로젝트 설명 나타나도록 변경

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 
+import { projectInfoState } from '../../stores/atom';
 import UserIcon from '../Icon/User';
-import { Container, LeftNav, RightNav } from './style';
+import { Container, LeftNav, ProjectDesc, ProjectInfo, ProjectTitle, RightNav } from './style';
 
 const userName = 'user';
 const userProfileImg = undefined;
 const userGithubId = 'user_github_id';
 
-const Header = () => (
-    <Container>
-        <LeftNav></LeftNav>
-        <RightNav>
-            <UserIcon userName={userName} githubId={userGithubId} image={userProfileImg} size="small" />
-        </RightNav>
-    </Container>
-);
+const Header = () => {
+    const projectInfo = useRecoilValue(projectInfoState);
+    return (
+        <Container>
+            <LeftNav>
+                <ProjectInfo>
+                    <ProjectTitle>{projectInfo.name}</ProjectTitle>
+                    <ProjectDesc>{projectInfo.desc}</ProjectDesc>
+                </ProjectInfo>
+            </LeftNav>
+            <RightNav>
+                <UserIcon userName={userName} githubId={userGithubId} image={userProfileImg} size="small" />
+            </RightNav>
+        </Container>
+    );
+};
 
 export default Header;
