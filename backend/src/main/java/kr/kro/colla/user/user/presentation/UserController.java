10e6230bd95bb53791e5ed1ffb3ac81a97f3e5a5
Subin Min <tnqls5417@naver.com>
2022-01-09 23:42:53 +0900

    feat: 프로젝트 생성시 관리자 프로젝트 멤버로 추가
    - UserService.findUserById()시에 사용자 없을시 예외 발생
    - pathVariable의 userId 사용자 객체 확인 후 프로젝트 생성
    - UserProjectService에 사용자와 프로젝트 연결 함수 추가

--- backend/src/main/java/kr/kro/colla/user/user/presentation/UserController.java
+++ backend/src/main/java/kr/kro/colla/user/user/presentation/UserController.java
@@ -2,8 +2,12 @@ package kr.kro.colla.user.user.presentation;
 
 import kr.kro.colla.project.project.domain.Project;
 import kr.kro.colla.project.project.service.ProjectService;
+import kr.kro.colla.user.user.domain.User;
 import kr.kro.colla.user.user.presentation.dto.CreateProjectRequest;
 import kr.kro.colla.user.user.presentation.dto.CreateProjectResponse;
+import kr.kro.colla.user.user.service.UserService;
+import kr.kro.colla.user_project.domain.UserProject;
+import kr.kro.colla.user_project.service.UserProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +19,15 @@ import javax.validation.Valid;
 @RequestMapping("/users")
 public class UserController {
     private final ProjectService projectService;
-
+    private final UserProjectService userProjectService;
+    private final UserService userService;
 
     @PostMapping("/{userId}/projects")
     public ResponseEntity<CreateProjectResponse> createProject(@PathVariable("userId") Long userId, @RequestBody @Valid CreateProjectRequest createProjectRequest){
+        User user = userService.findUserById(userId);
         Project project = projectService.createProject(userId, createProjectRequest);
+        userProjectService.addUserToProject(user, project);
+
         return ResponseEntity.ok(new CreateProjectResponse(project));
     }
 
