da859199cb696b023f214e625444421e3c4dbf4a
kykapple <kykapple@naver.com>
2022-01-15 01:37:36 +0900

    feat: 프로젝트 썸네일 등록 구현

--- frontend/src/components/Icon/Project/index.tsx
+++ frontend/src/components/Icon/Project/index.tsx
@@ -1,21 +1,52 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useEffect, useRef } from 'react';
 
-import { Container, ImageContainer } from './style';
+import EmptySrc from '../../../../public/assets/images/empty.png';
+import { Container, FileInput, Image } from './style';
 
 interface PropType {
-    projectName: string;
-    image?: string;
-    onClick?: () => void;
+    thumbnail: File | null;
+    // eslint-disable-next-line no-unused-vars
+    setThumbnail: (image: File) => void;
 }
 
-const ProjectIcon: FC<PropType> = ({ projectName, image, onClick }) => (
-    <div onClick={onClick}>
-        {image ? (
-            <ImageContainer image={image} />
-        ) : (
-            <Container>{projectName ? projectName[0].toUpperCase() : ''}</Container>
-        )}
-    </div>
-);
+const ProjectIcon: FC<PropType> = ({ thumbnail, setThumbnail }) => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const imgRef = useRef<HTMLImageElement | null>(null);
+
+    const handleImage = (event: ChangeEvent) => {
+        setThumbnail((event.target as HTMLInputElement)!.files![0]);
+    };
+
+    const onClickFileInput = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        inputRef.current?.click();
+    };
+
+    const previewImage = () => {
+        if (!thumbnail) {
+            imgRef.current!.src = EmptySrc;
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            imgRef.current!.src = fileReader.result as string;
+        };
+        fileReader.readAsDataURL(thumbnail);
+    };
+
+    useEffect(() => {
+        previewImage();
+    });
+
+    return (
+        <div>
+            <Container onClick={onClickFileInput}>
+                <FileInput type="file" name="thumbnail" ref={inputRef} onChange={handleImage} />
+                <Image ref={imgRef} />
+            </Container>
+        </div>
+    );
+};
 
 export default ProjectIcon;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
 
 import HomeImageSrc from '../../../public/assets/images/home-image.png';
-import { createProject } from '../../apis/user';
 import Header from '../../components/Header';
 import ProjectModal from '../../components/Modal/Project';
 import { SideBar } from '../../components/SideBar';
 import useModal from '../../hooks/useModal';
-import { projectDescState, projectNameState } from '../../stores/projectState';
 import { Container, HomeImage, ProjectNotice } from './style';
 
 const projects = [
@@ -21,24 +17,6 @@ const projects = [
 ];
 
 const Home = () => {
-    const setProjectName = useSetRecoilState(projectNameState);
-    const setProjectDesc = useSetRecoilState(projectDescState);
-    const history = useHistory();
-
-    const sendRequest = async (name: string, desc: string) => {
-        try {
-            const response = await createProject(name, desc);
-            const { name: projectName, description: projectDesc } = response.data;
-            setProjectName(projectName);
-            setProjectDesc(projectDesc);
-
-            history.push('/kanban', response.data);
-        } catch (e: any) {
-            // eslint-disable-next-line no-alert
-            window.alert(e.response.data.message);
-        }
-    };
-
     const { setModal, Modal } = useModal();
 
     return (
@@ -49,7 +27,7 @@ const Home = () => {
                 <HomeImage src={HomeImageSrc} />
                 <ProjectNotice onClick={setModal}>프로젝트를 추가해보세요!</ProjectNotice>
                 <Modal>
-                    <ProjectModal onClick={sendRequest} />
+                    <ProjectModal />
                 </Modal>
             </Container>
         </>
