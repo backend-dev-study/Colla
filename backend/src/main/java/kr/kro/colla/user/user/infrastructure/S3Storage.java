820c6538e2b61fed090c9602217937f47207c9b1
kykapple <kykapple@naver.com>
2022-01-15 18:19:59 +0900

    feat: 파일 업로드 실패 예외 구현

--- backend/src/main/java/kr/kro/colla/user/user/infrastructure/S3Storage.java
+++ backend/src/main/java/kr/kro/colla/user/user/infrastructure/S3Storage.java
@@ -3,6 +3,7 @@ package kr.kro.colla.user.user.infrastructure;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import kr.kro.colla.exception.exception.user.FileUploadException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -27,7 +28,7 @@ public class S3Storage {
                     .withCannedAcl(CannedAccessControlList.PublicRead));
             return s3Client.getUrl(bucket, fileName).toString();
         } catch (IOException e) {
-            throw new RuntimeException();
+            throw new FileUploadException();
         }
     }
 
