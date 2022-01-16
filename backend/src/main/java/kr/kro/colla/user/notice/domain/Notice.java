1f8e3a1f63b71730b15a8be8c6c94a7439fffc7d
Subin Min <tnqls5417@naver.com>
2022-01-16 23:58:51 +0900

    feat: Notice Builder 추가

--- backend/src/main/java/kr/kro/colla/user/notice/domain/Notice.java
+++ backend/src/main/java/kr/kro/colla/user/notice/domain/Notice.java
@@ -1,26 +1,39 @@
 package kr.kro.colla.user.notice.domain;
 
 import kr.kro.colla.user.notice.domain.converter.BooleanToIntegerConverter;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Entity
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
 public class Notice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Enumerated(EnumType.ORDINAL)
     private NoticeType noticeType;
 
     @Column
     private String mentionedUrl;
 
+    @NotNull
     @Convert(converter = BooleanToIntegerConverter.class)
     @Column
     private boolean isChecked;
 
+    @Builder
+    public Notice(NoticeType noticeType, boolean isChecked, String mentionedUrl){
+        this.noticeType = noticeType;
+        this.isChecked = isChecked;
+        this.mentionedUrl = mentionedUrl;
+    }
 }
