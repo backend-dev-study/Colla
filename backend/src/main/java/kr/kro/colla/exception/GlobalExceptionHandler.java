3d6e02ed0009778dc04b103a8457b5e0f0d4c80e
kykapple <kykapple@naver.com>
2022-01-17 13:59:45 +0900

    feat: 파일 업로드 실패 예외 처리 구현

--- backend/src/main/java/kr/kro/colla/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/kr/kro/colla/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package kr.kro.colla.exception;
 
 import kr.kro.colla.exception.exception.NotFoundException;
 import kr.kro.colla.exception.exception.auth.UnauthorizedException;
+import kr.kro.colla.exception.exception.user.FileUploadException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -15,7 +16,7 @@ import org.springframework.web.client.HttpClientErrorException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<ExceptionHandleResponse> handleBindException(BindException e) {
+    public ResponseEntity<ExceptionHandleResponse> handleBindException (BindException e) {
         FieldError fieldError = e.getFieldError();
         ExceptionHandleResponse response = new ExceptionHandleResponse(HttpStatus.BAD_REQUEST.value(), fieldError.getField() + " : " + fieldError.getDefaultMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -23,24 +24,31 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseEntity<ExceptionHandleResponse> handleHttpClientErrorException(HttpClientErrorException e) {
+    public ResponseEntity<ExceptionHandleResponse> handleHttpClientErrorException (HttpClientErrorException e) {
         ExceptionHandleResponse response = new ExceptionHandleResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(response);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ExceptionHandleResponse> handleUnauthorizedException(UnauthorizedException e) {
+    public ResponseEntity<ExceptionHandleResponse> handleUnauthorizedException (UnauthorizedException e) {
         ExceptionHandleResponse response = new ExceptionHandleResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(response);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ExceptionHandleResponse> handleNotFoundException(NotFoundException e) {
+    public ResponseEntity<ExceptionHandleResponse> handleNotFoundException (NotFoundException e) {
         ExceptionHandleResponse response = new ExceptionHandleResponse(e.getStatusCode().value(), e.getMessage());
-        return ResponseEntity.status(e.getStatusCode()).body(response);
+        return ResponseEntity.status(e.getStatusCode())
+                .body(response);
     }
 
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<ExceptionHandleResponse> handleFileUploadException (FileUploadException e) {
+        ExceptionHandleResponse response = new ExceptionHandleResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
 
 }
