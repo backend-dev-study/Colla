d93029585b21cd5bf12cbd4f3ebcfa77b284f99b
kykapple <kykapple@naver.com>
2022-01-25 02:47:41 +0900

    feat: 프로젝트 조회 API 연동

---
+++ frontend/src/apis/project.ts
@@ -0,0 +1,32 @@
+import { client } from './common';
+
+interface member {
+    displayName: string;
+    githubId: string;
+    avatar: string;
+}
+
+interface task {
+    id: number;
+    title: string;
+    managerName: string;
+    priority: number;
+}
+
+interface project {
+    id: number;
+    managerId: number;
+    name: string;
+    description: string;
+    thumbnail: string;
+    members: Array<member>;
+    tasks: {
+        [key: string]: Array<task>;
+    };
+}
+
+export const getProject = async (projectId: number) => {
+    const response = await client.get<project>(`/projects/${projectId}`);
+
+    return response;
+};
