e684d7117b1c3a79e368dd8a4f672a23cb4a95ce
kykapple <kykapple@naver.com>
2022-01-26 21:37:01 +0900

    feat: 스토리 - 프로젝트 연관관계 설정

--- backend/src/main/java/kr/kro/colla/story/domain/Story.java
+++ backend/src/main/java/kr/kro/colla/story/domain/Story.java
@@ -1,12 +1,16 @@
 package kr.kro.colla.story.domain;
 
+import kr.kro.colla.project.project.domain.Project;
 import kr.kro.colla.task.task.domain.Task;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
 @Getter
 @Entity
 public class Story {
@@ -19,12 +23,20 @@ public class Story {
     private String title;
 
     @Column
-    private String description;
-
-    @Column
     private String preStories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
 
     @OneToMany(mappedBy = "story", fetch = FetchType.LAZY)
     private List<Task> tasks = new ArrayList<>();
+
+    @Builder
+    public Story(String title, String preStories, Project project) {
+        this.title = title;
+        this.preStories = preStories;
+        this.project = project;
+    }
 
 }
