444547a8a673026690fcd6d49f809e283ad509c0
Subin Min <tnqls5417@naver.com>
2022-02-03 18:46:44 +0900

    feat: 알림 생성 함수 변경
    - 잘못된 요청 시의 예외 처리 추가
    - 알림 타입에 따라 다른 형태로 알림 생성
    - 테스트 추가 필요

--- backend/src/main/java/kr/kro/colla/user/notice/service/NoticeService.java
+++ backend/src/main/java/kr/kro/colla/user/notice/service/NoticeService.java
@@ -1,7 +1,11 @@
 package kr.kro.colla.user.notice.service;
 
+import kr.kro.colla.exception.exception.notice.NoticeBadRequestException;
 import kr.kro.colla.exception.exception.notice.NoticeNotFoundException;
+import kr.kro.colla.project.project.domain.Project;
+import kr.kro.colla.project.project.service.ProjectService;
 import kr.kro.colla.user.notice.domain.Notice;
+import kr.kro.colla.user.notice.domain.NoticeType;
 import kr.kro.colla.user.notice.domain.repository.NoticeRepository;
 import kr.kro.colla.user.notice.service.dto.CreateNoticeRequest;
 import kr.kro.colla.user.user.domain.User;
@@ -18,14 +22,30 @@ import javax.transaction.Transactional;
 public class NoticeService {
     private final NoticeRepository noticeRepository;
     private final UserService userService;
+    private final ProjectService projectService;
 
     public Notice createNotice(CreateNoticeRequest createNoticeRequest) {
         User user = userService.findUserById(createNoticeRequest.getReceiverId());
 
-        Notice notice = Notice.builder()
-                .noticeType(createNoticeRequest.getNoticeType())
-                .mentionedURL(createNoticeRequest.getMentionedURL())
-                .build();
+        Notice notice;
+        switch (createNoticeRequest.getNoticeType()){
+            case INVITE_USER:
+                Project project = projectService.findProjectById(createNoticeRequest.getProjectId());
+                notice = Notice.builder()
+                        .noticeType(NoticeType.INVITE_USER)
+                        .projectId(project.getId())
+                        .projectName(project.getName())
+                        .build();
+                break;
+            case MENTION_USER:
+                notice = Notice.builder()
+                        .noticeType(NoticeType.MENTION_USER)
+                        .mentionedURL(createNoticeRequest.getMentionedURL())
+                        .build();
+                break;
+            default:
+                throw new NoticeBadRequestException();
+        }
 
         Notice result = noticeRepository.save(notice);
         user.addNotice(result);
