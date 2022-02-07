d9e8e4638c4c5f1c67ffd77762e65e98f405ecc1
kykapple <kykapple@naver.com>
2022-02-07 23:56:50 +0900

    feat: 태스크 조회 API 구현

--- backend/src/main/java/kr/kro/colla/task/task/presentation/TaskController.java
+++ backend/src/main/java/kr/kro/colla/task/task/presentation/TaskController.java
@@ -1,10 +1,13 @@
 package kr.kro.colla.task.task.presentation;
 
 import kr.kro.colla.task.task.presentation.dto.CreateTaskRequest;
+import kr.kro.colla.task.task.presentation.dto.ProjectTaskResponse;
 import kr.kro.colla.task.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,6 +28,13 @@ public class TaskController {
 
         return ResponseEntity.created(redirectUrl)
                 .build();
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<ProjectTaskResponse> getTask(@PathVariable Long taskId) {
+        ProjectTaskResponse projectTaskResponse = taskService.getTask(taskId);
+
+        return ResponseEntity.ok(projectTaskResponse);
     }
 
 }
