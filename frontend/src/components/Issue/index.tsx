e33218108877c56f0e63eb1d926962f12d58fdc9
Subin Min <tnqls5417@naver.com>
2022-02-21 22:29:42 +0900

    feat: 하위 테스크 위한 CSS 수정

--- frontend/src/components/Issue/index.tsx
+++ frontend/src/components/Issue/index.tsx
@@ -1,11 +1,24 @@
 import React, { FC } from 'react';
-import { Wrapper } from './style';
+import { Attributes, Wrapper } from './style';
 
 interface PropType {
     story?: boolean;
     title: string;
 }
 
-const Issue: FC<PropType> = ({ story, title }) => <Wrapper story={story}>{title}</Wrapper>;
+const Issue: FC<PropType> = ({ story, title }) => (
+    <>
+        <Wrapper story={story}>
+            {title}
+            {!story ? (
+                <Attributes>
+                    <div>우선순위</div>
+                    <div>태그들</div>
+                    <div>담당자</div>
+                </Attributes>
+            ) : null}
+        </Wrapper>
+    </>
+);
 
 export default Issue;
--- frontend/src/components/Issue/style.tsx
+++ frontend/src/components/Issue/style.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 
 import { GREEN, WHITE } from '../../styles/color';
-import { WidthCenter } from '../../styles/common';
 
 interface Props {
     story?: boolean;
@@ -11,6 +10,10 @@ const BASE = 900;
 const EXTRA = 100;
 
 export const Wrapper = styled.div<Props>`
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
     width: ${({ story }) => (story ? `${BASE + EXTRA}px` : `${BASE}px`)};
     height: 40px;
     border-radius: 20px;
@@ -18,8 +21,11 @@ export const Wrapper = styled.div<Props>`
     margin-left: ${({ story }) => (story ? '0px' : `${EXTRA}px`)};
     margin-top: 13px;
     margin-bottom: 13px;
-    align-items: center;
     font-size: 20px;
+`;
 
-    ${WidthCenter}
+export const Attributes = styled.div`
+    position: absolute;
+    right: 20px;
+    display: flex;
 `;
