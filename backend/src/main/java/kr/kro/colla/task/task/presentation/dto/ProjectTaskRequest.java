769e2ef4c711a7b03a7f21d3215aae147a5e0603
kykapple <kykapple@naver.com>
2022-02-23 01:03:55 +0900

    feat: 우선순위에 따른 태스크 조회 API 테스트 코드 작성

--- backend/src/main/java/kr/kro/colla/task/task/presentation/dto/ProjectTaskRequest.java
+++ backend/src/main/java/kr/kro/colla/task/task/presentation/dto/ProjectTaskRequest.java
@@ -1,10 +1,12 @@
 package kr.kro.colla.task.task.presentation.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class ProjectTaskRequest {
