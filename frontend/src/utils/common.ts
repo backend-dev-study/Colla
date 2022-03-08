94fda9e58da26769f2bc842a37487e412cd9c947
Subin Min <tnqls5417@naver.com>
2022-03-08 23:40:18 +0900

    feat: 날짜 문자열 변환 함수 추가
    - YYYYMMDD 형식 문자열 Date 객체로 변환

--- frontend/src/utils/common.ts
+++ frontend/src/utils/common.ts
@@ -7,3 +7,12 @@ export const createFormData = (data: Object) => {
     Object.keys(data).forEach((key) => formData.append(key, data[key]!));
     return formData;
 };
+
+export const YYYYMMDDToDate = (str: string) => {
+    const date = new Date(
+        parseInt(str.slice(0, 4), 10),
+        parseInt(str.slice(4, 6), 10) - 1,
+        parseInt(str.slice(6, 8), 10),
+    );
+    return date;
+};
