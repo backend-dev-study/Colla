f89d2092f9b9f23834d081c32735bf98753b9789
Subin Min <tnqls5417@naver.com>
2022-03-11 19:14:43 +0900

    feat: 장소 추가 모달 UI
    - Place 속성 정해지고 컴포넌트화

--- frontend/src/pages/Map/index.tsx
+++ frontend/src/pages/Map/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
+import PlaceModal from '../../components/Modal/Place';
 import Template from '../../components/Template';
+import useModal from '../../hooks/useModal';
 import { Container, AddPlace, PlaceList, Place } from './style';
 
-const Map = () => (
-    <Template>
-        <Container>
-            <AddPlace>모임 장소 추가</AddPlace>
-            <PlaceList>
-                <Place>장소1</Place>
-                <Place>장소2</Place>
-                <Place>장소3</Place>
-                <Place>장소3</Place>
-                <Place>장소3</Place>
-            </PlaceList>
-        </Container>
-    </Template>
-);
+const Map = () => {
+    const { Modal, setModal } = useModal();
+
+    return (
+        <Template>
+            <Container>
+                <AddPlace onClick={setModal}>모임 장소 추가</AddPlace>
+                <PlaceList>
+                    <Place>장소1</Place>
+                    <Place>장소2</Place>
+                    <Place>장소3</Place>
+                    <Place>장소3</Place>
+                    <Place>장소3</Place>
+                </PlaceList>
+            </Container>
+            <Modal>
+                <PlaceModal />
+            </Modal>
+        </Template>
+    );
+};
 
 export default Map;
