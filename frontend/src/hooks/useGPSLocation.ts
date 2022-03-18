b3c6e04781d0699c2d15e0a756a1678889736449
Subin Min <tnqls5417@naver.com>
2022-03-18 19:58:38 +0900

    feat: GPS 위치 정보 조회 위한 훅 추가
    - Geolocation 이용
    - 위치 정보 {lng: number, lat: number} 형식으로 반환
    - 위치 정보 조회할 수 없을 시 {lng: undefined, lat:undefined} 로 반환

---
+++ frontend/src/hooks/useGPSLocation.ts
@@ -0,0 +1,22 @@
+import { toast } from 'react-toastify';
+
+const GPS_NOT_SUPPORT = '현재 기기가 위치 정보 추적을 지원하지 않습니다.';
+const GPS_ERROR = '위치 정보 불러오기에 실패했습니다.';
+
+const useGPSLocation = () => {
+    if (navigator.geolocation) {
+        return new Promise((resolve) => {
+            navigator.geolocation.getCurrentPosition(
+                (position: GeolocationPosition) =>
+                    resolve({ lng: position.coords.longitude, lat: position.coords.latitude }),
+                () => {
+                    toast.error(GPS_ERROR);
+                    resolve({ lng: undefined, lat: undefined });
+                },
+            );
+        });
+    }
+    toast.error(GPS_NOT_SUPPORT);
+};
+
+export default useGPSLocation;
