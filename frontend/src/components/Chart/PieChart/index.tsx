b1ef6620a2262a800dfeb213aef3f733ae592a1a
Subin Min <tnqls5417@naver.com>
2022-03-23 00:47:26 +0900

    feat: 도넛 차트 추가 수정
    - 위치 및 크기 조정

--- frontend/src/components/Chart/PieChart/index.tsx
+++ frontend/src/components/Chart/PieChart/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
 import Circle from './Circle';
+import { SVG } from './style';
 
 const RADIUS = 70;
 const STROKE_WIDTH = 30;
 
 const PieChart = () => (
     <>
-        <svg viewBox="0 0 200 200">
+        <SVG viewBox="0 0 200 200">
             <Circle progress={100} radius={RADIUS} strokeColor={'#ffffff'} strokeWidth={STROKE_WIDTH} />
             <Circle progress={100} radius={RADIUS} strokeColor={'#6f31fe'} strokeWidth={STROKE_WIDTH} />
             <Circle progress={70} radius={RADIUS} strokeColor={'#1131fe'} strokeWidth={STROKE_WIDTH} />
             <Circle progress={50} radius={RADIUS} strokeColor={'#6ef145'} strokeWidth={STROKE_WIDTH} />
             <Circle progress={40} radius={RADIUS} strokeColor={'#bd35fe'} strokeWidth={STROKE_WIDTH} />
             <Circle progress={10} radius={RADIUS} strokeColor={'#fe31fe'} strokeWidth={STROKE_WIDTH} />
-        </svg>
+        </SVG>
     </>
 );
 
