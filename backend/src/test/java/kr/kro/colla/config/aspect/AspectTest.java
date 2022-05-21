39f9a14816011bd4d970d7c204adb7fcf03abbb7
Subin Min <tnqls5417@naver.com>
2022-05-21 19:01:08 +0900

    refactor: 필요없는 테스트 구성 삭제
    - @SpringBootTest -> 단위테스트로 변경

--- backend/src/test/java/kr/kro/colla/config/aspect/AspectTest.java
+++ backend/src/test/java/kr/kro/colla/config/aspect/AspectTest.java
@@ -1,22 +1,12 @@
 package kr.kro.colla.config.aspect;
 
-import kr.kro.colla.task.task.service.TaskService;
-import org.junit.jupiter.api.BeforeAll;
+import kr.kro.colla.utils.ProxyInitialized;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Import(AspectTestConfig.class)
-@SpringBootTest
 public class AspectTest {
-    @Autowired
-    private TaskService taskService;
-
     AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
 
     @BeforeEach
@@ -26,19 +16,36 @@ public class AspectTest {
 
     @Test
     void 조건에_해당되면_포인트컷과_매칭된다() throws NoSuchMethodException {
-        boolean isMatch = pointcut.matches(TestForAspect.class.getMethod("correctMethod", Long.class, String.class), TestForAspect.class);
+        boolean isMatch = pointcut.matches(AspectTarget.class.getMethod("correctMethod", Long.class, String.class), AspectTarget.class);
         assertThat(isMatch).isTrue();
     }
 
     @Test
     void 어노테이션이_명시되지_않으면_매칭되지_않는다() throws NoSuchMethodException {
-        boolean isMatch = pointcut.matches(TestForAspect.class.getMethod("annotationWrongMethod", Long.class), TestForAspect.class);
+        boolean isMatch = pointcut.matches(AspectTarget.class.getMethod("annotationWrongMethod", Long.class), AspectTarget.class);
         assertThat(isMatch).isFalse();
     }
 
     @Test
     void 파라미터_타입이_올바르지_않으면_매칭되지_않는다() throws NoSuchMethodException {
-        boolean isMatch = pointcut.matches(TestForAspect.class.getMethod("parameterWrongMethod", String.class), TestForAspect.class);
+        boolean isMatch = pointcut.matches(AspectTarget.class.getMethod("parameterWrongMethod", String.class), AspectTarget.class);
         assertThat(isMatch).isFalse();
+    }
+}
+
+class AspectTarget {
+
+    @ProxyInitialized(target = "Test")
+    public void correctMethod(Long id, String other) {
+
+    }
+
+    @ProxyInitialized(target = "Test")
+    public void parameterWrongMethod(String other) {
+
+    }
+
+    public void annotationWrongMethod(Long id) {
+
     }
 }
